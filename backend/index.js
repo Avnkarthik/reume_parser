cb587c235205d45687ca6246cad8b08a78fa03de
Avnkarthik <annavarapukarthik0@gmail.com>
2026-01-21 21:07:50 +0530

    Server

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import multer from "multer";
+import express from "express"
+import pkg from "./ReumeParser.cjs";
+const {Parser}=pkg;
+import cors from "cors"
+import path from "path";
+const app=new express();
+app.use(express.json());
+app.use(cors({
+    origin:"*",
+}))
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"uploads");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+"-"+file.originalname);
+    }
+});
+const fileFilter=(req,file,cb)=>{
+    if(file.mimetype==="application/pdf" ||  file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || file.mimetype==="text/plain")
+        cb(null,true);
+    else cb(new Error("Unsupported file type"),false);
+
+}
+const upload=multer({
+    storage:storage,
+    fileFilter:fileFilter,
+    limits:{fileSize:5*1024*1024}
+
+    
+})
+app.post("/uploadResume",upload.single("Resume"),Parser);
+
+app.listen("8010",()=>console.log("LIstening on port 8010"));
